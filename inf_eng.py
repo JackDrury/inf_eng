845f702fc6e95cd164b7f918103879518acecba8
Jack Drury <jack@drury.com>
2020-05-31 18:06:32 +1000

    imperative inference done

--- inf_eng.py
+++ inf_eng.py
@@ -1,32 +1,79 @@
-from collections import deque
+"""
+We assume there are no loops and that each rule has a single atomic conclusion
+
+For example1:
+we start with
+              a,b -> c
+            c,d,e -> f
+
+# We want to use array indexing for because it is (O(1)) faster
+# than associative array / hashmap access
+# So we will translate each atom to an integer
+
+atom_map = {'a':0,'b':1,'c':2,'d':3,'e':4,'f':5}
+
+# We describe the rules as a pair with the first element being
+# the number of False premises (all premises are assumed false 
+# to begin with). The second element is the atomic prop conclusion.
 
 rules = [[2,2],[3,5]]
 
-atoms = [(False,[0]),(False,[0]),(False,[1]),(False,[1]),(False,[1]),(False,[])]
+# An atom has a truth value (assumed to be false at the start)
+# and a list of rules it is a premise for. Rules are referred to by 
+# their array index. That is all sorted in pre-processing / pretreatment.
 
-atom_map = {"a":0,"b":1,"c":2,"d":3,"e":4,"f":5}
+atoms = [[False,[0]],[False,[0]],[False,[1]],[False,[1]],[False,[1]],[False,[]]]
 
-pretreated = (rules,atoms,atom_map)
+# our pretreated structure is then the triple:
+pretreated = (rules, atoms, atom_map)
 
-assumps = ["a","b",'d','e']
+# From the example we have the assumptions:
+assumps = ['a','b','d','e']
 
-discovered = []
 
+
+For example2:
+
+We want an example where at least one atom appears in more than one premise. So we have:
+
+           a,b -> c
+       a,c,d,e -> f
+         a,c,f -> g
+
+atom_map = {'a':0,'b':1,'c':2,'d':3,'e':4,'f':5,'g':6}
+
+rules = [[2,2],[4,5],[3,6]]
+
+atoms = [[False,[0,1,2]],[False,[0]],[False,[1,2]],[False,[1]],[False,[1]],[False,[2]],[False,[]]]
+
+pretreated = (rules, atoms, atom_map)
+
+assumps = ['a','b','d','e']
+
+
+"""
+from collections import deque
 
 
 def infer(pretreated, assumps):
+    rules      = pretreated[0]
+    atoms      = pretreated[1]
+    atom_map   = pretreated[2]
+    discovered = []
     q = deque()
-    for i in assumps:
-        q.appendleft(atom_map[i])
-    while q:
-        a = q.pop()
-        if not atoms[a][0]:
+    for i in assumps:                    # put translation of all initial
+        if i in atom_map.keys():         # assumptions in a queue
+            q.appendleft(atom_map[i])    # (only if it appears in the rules)
+    while q:                             # While the queue is not empty
+        a = q.pop()                      # pop value of an atom
+        if not atoms[a][0]:              # set it to True if it isn't already
             atoms[a][0] = True
-            for j in atoms[a][1]:
+            for j in atoms[a][1]:        # reduce each of the rules it is a premise for
                 rules[j][0] -= 1
-                if rules[j][0] == 0:
+                if rules[j][0] == 0:     # If the rule has no false premises add its conclusion
                     discovered.append(rules[j][1])
                     q.appendleft(rules[j][1])
-    print(discovered)
     return discovered
-            
+
+# Can translate the discovered atoms back to a string/name if desired.
+     
